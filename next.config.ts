e7b40922ccab1577a00cd1079cfcd6e7deed5713
fredocto <fredoctovian@gmail.com>
2026-09-19 02:10:29 +0700

    fix: duplicate nextConfig identifier

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'bridge.pormaesa.my.id',
-          },
-        ],
-        destination: '/:path*',
-      },
-    ]
-  },
+  /* config options here */
 }
 
 export default nextConfig
