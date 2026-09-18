159378a858dc541c795dcc6d69c5a44f5671137d
fredocto <fredoctovian@gmail.com>
2026-09-19 02:01:52 +0700

    fix: clear root dir and update next config

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,23 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'bridge.pormaesa.my.id',
+          },
+        ],
+        destination: '/:path*',
+      },
+    ]
+  },
+}
+
+export default nextConfig
